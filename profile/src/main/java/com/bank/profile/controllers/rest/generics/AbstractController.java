da33754c286bec932ee0ec4b02e5d0d7301654b5
troshin.matvey@yandex.ru <108873914+onodaHiroo@users.noreply.github.com>
2024-10-11 16:02:43 +0300

    Добавил аннотации для сваггера

--- profile/src/main/java/com/bank/profile/controllers/rest/generics/AbstractController.java
+++ profile/src/main/java/com/bank/profile/controllers/rest/generics/AbstractController.java
@@ -3,6 +3,7 @@ package com.bank.profile.controllers.rest.generics;
 import com.bank.profile.entity.abstracts.AbstractEntity;
 import com.bank.profile.mappers.generics.BaseMapper;
 import com.bank.profile.service.generics.BaseCrudService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,18 +27,21 @@ public abstract class AbstractController<
 
     @Override
     @GetMapping("")
+    @Tag(name = "Получение всех записей", description = "Получение всех записей типа предпоследнего эндпоинта")
     public ResponseEntity<List<DTO>> showAll() {
         return new ResponseEntity<>(service.readAll(), HttpStatus.OK);
     }
 
     @Override
     @GetMapping("/show")
+    @Tag(name = "Получение одной записи", description = "Получение одной конкретной записи типа предпоследнего эндпоинта (id указывается в параметрах get запроса)")
     public ResponseEntity<DTO> showById(@RequestParam(value = "id") Long id) {
         return new ResponseEntity<>(service.read(id), HttpStatus.OK);
     }
 
     @Override
     @GetMapping("/delete")
+    @Tag(name = "Удаление одной записи", description = "Удаление одной конкретной записи типа предпоследнего эндпоинта (id указывается в параметрах get запроса)")
     public ResponseEntity<HttpStatus> deleteById(@RequestParam(value = "id") Long id) {
         service.delete(id);
         return ResponseEntity.ok(HttpStatus.OK);
@@ -45,6 +49,7 @@ public abstract class AbstractController<
 
     @Override
     @PostMapping("/new")
+    @Tag(name = "Создание одной записи", description = "Создание одной конкретной записи типа предпоследнего эндпоинта (id не указывается)")
     public ResponseEntity<HttpStatus> save(@RequestBody DTO dto) {
         try {
             service.create(dto);
@@ -56,6 +61,7 @@ public abstract class AbstractController<
 
     @Override
     @PostMapping("/update")
+    @Tag(name = "Изменение одной записи", description = "Изменение одной конкретной записи типа предпоследнего эндпоинта (id указывается в теле post запроса)")
     public ResponseEntity<HttpStatus> edit(@RequestBody DTO dto) {
         try {
             service.update(dto);
