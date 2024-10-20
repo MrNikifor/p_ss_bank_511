72656412e742ff444eb611849a95f6498c8c5c79
troshin.matvey@yandex.ru <108873914+onodaHiroo@users.noreply.github.com>
2024-10-20 21:09:31 +0300

    1) Добавил в exception handler отлов ошибки валидации чтобы проверять их в тестах в будущем. 2) В контроллере убрал лишний try/catch. 3) Удалил тест валидатора  чтобы переделать к следующему merge request'у

--- profile/src/main/java/com/bank/profile/controllers/rest/generics/AbstractController.java
+++ profile/src/main/java/com/bank/profile/controllers/rest/generics/AbstractController.java
@@ -5,6 +5,7 @@ import com.bank.profile.mappers.generics.BaseMapper;
 import com.bank.profile.service.generics.BaseCrudService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -14,7 +15,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.xml.bind.ValidationException;
 import java.util.List;
 
 @AllArgsConstructor
@@ -46,26 +46,20 @@ public abstract class AbstractController<ENTITY extends AbstractEntity, DTO, SER
     }
 
     @Override
+    @SneakyThrows
     @PostMapping("/new")
     @Tag(name = "Создание одной записи", description = "Создание одной конкретной записи типа предпоследнего эндпоинта (id не указывается)")
     public ResponseEntity<String> save(@RequestBody DTO dto) {
-        try {
-            service.create(dto);
-        } catch (ValidationException e) {
-            return new ResponseEntity<>("Ошибка при создании сущности: " + e.getMessage(), HttpStatus.BAD_REQUEST);
-        }
+        service.create(dto);
         return ResponseEntity.ok("Успешно создано");
     }
 
     @Override
+    @SneakyThrows
     @PutMapping("/update")
     @Tag(name = "Изменение одной записи", description = "Изменение одной конкретной записи типа предпоследнего эндпоинта (id указывается в теле post запроса)")
     public ResponseEntity<String> edit(@RequestBody DTO dto) {
-        try {
-            service.update(dto);
-        } catch (ValidationException e) {
-            return new ResponseEntity<>("Ошибка при обновлении сущности: " + e.getMessage(), HttpStatus.BAD_REQUEST);
-        }
+        service.update(dto);
         return ResponseEntity.ok("Успешно обновлено");
     }
 }
--- profile/src/main/java/com/bank/profile/handlers/RestExceptionHandler.java
+++ profile/src/main/java/com/bank/profile/handlers/RestExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import javax.validation.ValidationException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,6 +18,13 @@ public class RestExceptionHandler {
         Map<String, String> response = new HashMap<>();
         response.put("error", ex.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    }
+
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity<Map<String, String>> handleValidationException(ValidationException ex) {
+        Map<String, String> response = new HashMap<>();
+        response.put("error", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
 
     @ExceptionHandler(Exception.class)
